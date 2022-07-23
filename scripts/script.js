f02abecb2d62957aace239d60c7a2df7d1af986d
Malcolm Tsang <61926619+s3333925@users.noreply.github.com>
2022-07-23 17:17:21 +1000

    before nav

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,5 @@
+/*
+
 // Function create sticky nav bar.
 // W3Schools. 2022. Code. https://www.w3schools.com/howto/howto_js_navbar_sticky.asp
 function stickyNavbar() {
@@ -17,9 +19,13 @@ function stickyNavbar() {
   }
 }
 
+*/
+
 // Function to change carousel image on home page.
 // W3Schools. 2022. Code. https://www.w3schools.com/howto/howto_js_slideshow.asp
 let slideIndex = 1;
+
+showSlides(slideIndex);
 
 function plusSlides(n) {
   showSlides((slideIndex += n));
@@ -49,6 +55,10 @@ function showSlides(n) {
   dots[slideIndex - 1].className += " active";
 }
 
+// // Function to chech if any user data is stored in local storage
+
+checkUserDetails();
+
 function checkUserDetails() {
   if (localStorage.getItem("name") === null) {
     document.getElementById("name").value = "";
@@ -68,4 +78,30 @@ function forgetOrRememberMe() {
     localStorage.name = document.getElementById("name").value;
     localStorage.email = document.getElementById("email").value;
   }
+}
+
+// ----------------------------------
+
+//Get the button
+let mybutton = document.getElementById("button-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  console.log("vbn");
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
