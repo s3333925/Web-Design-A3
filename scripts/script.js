8b331592711a5a09456859c4dc4a246d44be41e9
Malcolm Tsang <61926619+s3333925@users.noreply.github.com>
2022-07-25 18:43:21 +1000

    update

--- scripts/script.js
+++ scripts/script.js
@@ -1,37 +1,3 @@
-// Function to change carousel image on home page.
-// W3Schools. 2022. Code. https://www.w3schools.com/howto/howto_js_slideshow.asp
-let slideIndex = 1;
-
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides((slideIndex += n));
-}
-
-function currentSlide(n) {
-  showSlides((slideIndex = n));
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
-}
-
 // Forget or remember user contact information
 
 checkUserDetails();
